55d991c7ddfcfb847e14cece38f06c753df65b0d
Tarek-Bohdima <tarek.buhdeima@gmail.com>
2024-03-10 17:16:07 +0200

    update secret api key location

--- app/src/test/java/com/tarek/asteroidradar/ExampleUnitTest.kt
+++ app/src/test/java/com/tarek/asteroidradar/ExampleUnitTest.kt
@@ -29,9 +29,8 @@
 
 package com.tarek.asteroidradar
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -41,6 +40,6 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+        assertEquals(4, 24 + 2)
     }
 }
